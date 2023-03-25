dff199b51cf45db35e3ea6d1c9d991b8e01ea19f
quinnsest <eolaosho79@gmail.com>
2023-03-24 17:49:21 -0700

    multiplies two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/**
+ * mul - multiplies 2 numbers
+ *
+ * @a: input for first value
+ *
+ * @b: input for second value
+ *
+ * Return: Result of a * b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
