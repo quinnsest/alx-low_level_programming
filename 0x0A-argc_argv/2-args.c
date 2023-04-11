bb29375145b4197710dd7f16e322f6c9fa837201
quinnsest <eolaosho79@gmail.com>
2023-04-10 18:58:30 -0700

    prints all arguement received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the number of arguments
+ *        passed into it
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
+	return (0);
+}
