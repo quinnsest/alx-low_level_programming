4d74aa906c00e559aa9f9613cb2001c60964a4e5
quinnsest <eolaosho79@gmail.com>
2023-04-20 10:51:55 -0700

    0. Whats my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,10 @@
  * print_name - function to print name
  *
  * @name: name to print
- * @f: function pointer that doesn't return anything
+ * @f: function pointer tht return zero
  *
  * Return: Always 0 (Success)
-*/
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
